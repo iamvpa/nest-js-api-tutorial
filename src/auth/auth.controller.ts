28d2e2d70ae4cfd61dde8766a407535145c5b962
iamvpa <vpacorp@hotmail.com>
2022-07-18 11:50:17 +0530

    q4gqg

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,15 @@
-import {Controller } from "@nestjs/common";
+import {Controller, Post } from "@nestjs/common";
 import {AuthService} from './auth.service';
-@Controller({})
+@Controller('auth') //good practice to put a global route
 export class AuthController{
     constructor(private authService:AuthService){}
+    @Post('signup')
+    signup(){
+        return 'I am signed up'
+    }
+
+    @Post('signin') //defining route
+    signin(){
+        return 'I am signed in'
+    }
 }
